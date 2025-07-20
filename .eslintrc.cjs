7aa09ef85ea49367bc77f44789c99b60e0472b6b
Qi <bobo_xu@hotmail.com>
2025-07-20 21:37:59 +0200

    Initialize pnpm monorepo with web and server

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    es2020: true,
+    node: true,
+    browser: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ web/src/App.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export function App() {
+  return <h1>Guandan Online</h1>;
+}
